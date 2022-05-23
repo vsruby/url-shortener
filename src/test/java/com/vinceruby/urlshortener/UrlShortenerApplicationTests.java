542aed698d9da9913df17b9b2cdf0782ada2175c
Vince Ruby <vsruby91@gmail.com>
2022-05-23 07:21:59 -0400

    Update smoke test

--- src/test/java/com/vinceruby/urlshortener/UrlShortenerApplicationTests.java
+++ src/test/java/com/vinceruby/urlshortener/UrlShortenerApplicationTests.java
@@ -1,13 +1,20 @@
 package com.vinceruby.urlshortener;
 
+import com.vinceruby.urlshortener.controller.RedirectionController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class UrlShortenerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private RedirectionController redirectionController;
 
+	@Test
+	void itShouldPassTheSmokeTest() {
+		assertNotNull(redirectionController);
+	}
 }
